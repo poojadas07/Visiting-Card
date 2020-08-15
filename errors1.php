9635b85a359f53ff1b62d2cfefcd585925f5af98
poojadas07 <poojadas04kv@gmail.com>
2020-08-15 20:59:07 +0530

    Alignment

--- errors1.php
+++ errors1.php
@@ -1,7 +1,9 @@
 <?php  if (count($errors1) > 0) : ?>
+
   <div class="error">
   	<?php foreach ($errors1 as $error) : ?>
   	  <p><?php echo $error ?></p>
   	<?php endforeach ?>
   </div>
+  
 <?php  endif ?>
